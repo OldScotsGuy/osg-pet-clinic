27eda6dd9d01c9ba55cbe8cf1347667911384660
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-04 23:00:35 +0100

    Added base entity, Closes #10.

--- pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package org.nickharle.osgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package org.nickharle.osgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
