355b46bea6d5aaf8eadd0b711a854ce53116c37b
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-11 21:56:34 +0100

    Added vets list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/org/nickharle/osgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/nickharle/osgpetclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package org.nickharle.osgpetclinic.controllers;
 
+import org.nickharle.osgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findall());
+
         return "vets/index";
     }
 
