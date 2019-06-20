9c652cdc9f585e332ad2c284dc9854e2ef92fab1
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-20 20:50:31 +0100

    Added PetType Service. Closes #29.

---
+++ pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package org.nickharle.osgpetclinic.services;
+
+import org.nickharle.osgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package org.nickharle.osgpetclinic.services.map;
+
+import org.nickharle.osgpetclinic.model.PetType;
+import org.nickharle.osgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findall() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
