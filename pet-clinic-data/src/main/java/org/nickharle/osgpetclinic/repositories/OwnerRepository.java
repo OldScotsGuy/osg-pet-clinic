2c22b1ac3a884f275dd31dcb20f4d55c569c661d
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-23 12:29:18 +0100

    Created Spring Data JPA Service For Owners. Closes #40.

--- pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/org/nickharle/osgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import org.nickharle.osgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
 }
