cafc12dc27c31efbd593be378d9b62d98dbc8d4d
OldScotsGuy <nickharle@gmail.com>
2019-07-10 14:01:30 +0100

    Added pet forms. #56

---
+++ pet-clinic-web/src/test/java/org/nickharle/osgpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,66 @@
+package org.nickharle.osgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.nickharle.osgpetclinic.model.Owner;
+import org.nickharle.osgpetclinic.model.PetType;
+import org.nickharle.osgpetclinic.services.OwnerService;
+import org.nickharle.osgpetclinic.services.PetService;
+import org.nickharle.osgpetclinic.services.PetTypeService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class PetControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1L).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(petController).build();
+    }
+
+    public void initCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findall()).thenReturn(petTypes);
+
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("/pets/createOrUpdatePetForm"));
+    }
+
+}
