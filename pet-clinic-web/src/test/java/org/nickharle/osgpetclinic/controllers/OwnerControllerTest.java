6d21a9ea9cab327fde7c0fd4c86743e6a87f8bd5
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-07-13 18:59:27 +0100

    Writen test for empty find owner last name to return all

--- pet-clinic-web/src/test/java/org/nickharle/osgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/org/nickharle/osgpetclinic/controllers/OwnerControllerTest.java
@@ -80,6 +80,20 @@ class OwnerControllerTest {
                 .andExpect(model().attribute("selections", hasSize(2)));
     }
 
+
+    @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build()));
+
+        mockMvc.perform(get("/owners")
+                .param("lastName",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));;
+    }
+
     @Test
     public void processFindFormReturnOne() throws Exception {
         when(ownerService.findAllByLastNameLike(anyString()))
@@ -89,6 +103,8 @@ class OwnerControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/owners/1"));
     }
+
+
 
     @Test
     // Test empty object assigned to form and correct form displayed
