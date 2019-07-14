46fdbaf7e603dde6dadd020423f5c7470765787b
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-07-14 12:24:55 +0100

    Implemented JSON end point for vets. Closes #68.

--- pet-clinic-web/src/main/java/org/nickharle/osgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/nickharle/osgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package org.nickharle.osgpetclinic.controllers;
 
+import org.nickharle.osgpetclinic.model.Vet;
 import org.nickharle.osgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,6 +26,12 @@ public class VetController {
         model.addAttribute("vets", vetService.findall());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    // @ResponseBody is format of return, which by default is JSON
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findall();
     }
 
 }
